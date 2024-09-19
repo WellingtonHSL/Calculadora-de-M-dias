6a648ba503cb52cfe85d854ed53e4cad8fa79aaf
Wellington Henrique <wellingtonh_2012@hotmail.com>
2024-09-19 19:08:56 -0300

    Update calc_media.php

--- calc_media.php
+++ calc_media.php
@@ -3,6 +3,7 @@
 
         $media_final = '';
         $frase = '';
+        $conceito = '';
 
         $num_1 = $_POST['num_1'];
         $num_2 = $_POST['num_2'];
@@ -13,17 +14,21 @@
 
         if($media_final >= 9){
             $frase = "Aprovado com Louvor!";
+            $conceito = "A";
         } elseif ($media_final >= 7 && $media_final < 9) {
             $frase = "Aluno Aprovado";
+            $conceito = "B";
         } elseif ($media_final >= 4 && $media_final < 7){
             $frase = "Recuperação, sua chance de passar!";
+            $conceito = "C";
         } else {
             $frase = "Poxa vida, vamos tentar novamente ano que vem!";
+            $conceito = "D";
         }
         
         $_SESSION['media_final'] = $media_final;
         $_SESSION['frase'] = $frase;
-
+        $_SESSION['conceito'] = $conceito;
     header('Location: index.php');
     exit;
  ?>
