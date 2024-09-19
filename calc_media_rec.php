6b9ba3d45a3a0b31c08f8fdf2815da16281c5594
Wellington Henrique <wellingtonh_2012@hotmail.com>
2024-09-19 20:27:42 -0300

    Update calc_media_rec.php

--- calc_media_rec.php
+++ calc_media_rec.php
@@ -1,23 +1,43 @@
-<?php
-    session_start();
-
-        $media_final = $_SESSION['media_final'];
-        $media_final_rec = '';
-        $frase_rec = '';
-
-        $num_5 = (float) $_POST['num_5'];
-
-        $media_final_rec = ($num_5 + $media_final)/2;
-
-        if($media_final_rec >= 7){
-            $frase_rec = "Aluno Aprovado!";
-        } else{
-            $frase_rec = "Poxa vida, vamos tentar novamente ano que vem!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            text-align: center;
         }
+    </style>
+    <title>Calculadora</title>
+</head>
+    <header>
+        <h1>Calculadora de Médias</h1>
+    </header>
+    <body>
+        <fieldset>
+        <?php
+                $media_final = $_POST['media_final'];
+                $media_final_rec = '';
+                $frase_rec = '';
 
-        $_SESSION['media_final_rec'] = $media_final_rec;
-        $_SESSION['frase_rec'] = $frase_rec;
+                $num_5 = (float) $_POST['num_5'];
 
-    header('Location: index.php');
-    exit;
-?>
+                $media_final_rec = ($num_5 + $media_final);
+
+                if($media_final_rec >= 10){
+                    $frase_rec = "Aluno Aprovado!";
+                } else{
+                    $frase_rec = "Poxa vida, vamos tentar novamente ano que vem!";
+                }
+        ?>
+                <h3>Média Final Recuperação: <?php echo htmlspecialchars($media_final_rec)?></h3>
+                <p><?php echo htmlspecialchars($frase_rec)?></p>
+        </fieldset>
+    </body>
+</html>
